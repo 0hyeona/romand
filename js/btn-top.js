b40a2bd42580983df7f6302d3ee797211b5e688d
hyeona <01.hyeona@gmail.com>
2026-09-03 22:39:10 +0900

    fix: 스크롤 플로팅 버튼 로직 수정

--- js/btn-top.js
+++ js/btn-top.js
@@ -1,17 +1,18 @@
-let lastScrollTop = 0;
-
-window.addEventListener('scroll', () => {
-    let ScrollTop = window.pageYOffset;
-    if (ScrollTop < lastScrollTop) {
-        btnTop.classList.add('on');
-    } else {
-        btnTop.classList.remove('on');
-    }
-    lastScrollTop = ScrollTop;
-});
-
-
 const btnTop = document.querySelector('.btn-top');
-btnTop.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}) 
+
+if (btnTop) {
+    btnTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            btnTop.classList.add('on');
+        } else {
+            btnTop.classList.remove('on');
+        }
+    });
+}
