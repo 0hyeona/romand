f478cf8b60b0f87cefdd7065294a7a4cde8d828d
ycr <chaerin7272@gmail.com>
2026-09-04 15:38:38 +0900

    베스트셀러 반응형 완료/카드리스트모델img 1번 png->jpg 변환

--- js/product-bestseller.js
+++ js/product-bestseller.js
@@ -1,62 +1,70 @@
-let productArray = [
+const productArray = [
     {
         pid: 0,
-        pname: '모노 패브릭 소파',
-        pdesc: `부드러운 패브릭과 여유로운 좌석으로 완성한 편안한 3인 소파`,
-        price: 2180000,
-        pdiscount: 0.25,
-        priceDiscount: 1635000,
-        plike: 86,
-        pthumbFileName: 'con3-1.jpg'
+        pname: '쥬시 플래시 립 오일(+롬앤 립 랜덤 1종 증정)',
+        price: 14000,
+        pdiscount: 0.22,
+        priceDiscount: 10900,
+        plipImgName: 'lip-list-img-1.jpg',
+        plipModelName: 'lip-list-model-img-1.jpg',
+        // 상품별 컬러칩 생성과 +Color 개수 계산에 사용하는 색상 데이터
+        pcolors: [
+            '#9A777E', '#B1848C', '#C999A1', '#DCB4BA', '#EAD8DC',
+            '#724047', '#864A51', '#A55F68', '#BD7580', '#D08C96',
+            '#E2ABB3', '#F1CED3',
+        ],
+        badgeBest: 'off',
+        badgeNew: 'on',
     },
     {
         pid: 1,
-        pname: '오브 라운지 체어',
-        pdesc: `둥근 실루엣과 포근한 쿠션이 돋보이는 데일리 라운지 체어`,
-        price: 680000,
-        pdiscount: 0.3,
-        priceDiscount: 476000,
-        plike: 124,
-        pthumbFileName: 'con3-2.jpg'
+        pname: '롬앤 글래스팅 컬러 글로스 컬러',
+        price: 13000,
+        pdiscount: 0.28,
+        priceDiscount: 9400,
+        plipImgName: 'lip-list-img-2.jpg',
+        plipModelName: 'lip-list-model-img-2.jpg',
+        // 상품별 컬러칩 생성과 +Color 개수 계산에 사용하는 색상 데이터
+        pcolors: [
+            '#734149', '#92545C', '#AE6A73', '#D48F9A', '#E8D5D9',
+            '#B54D65',
+        ],
+        badgeBest: 'on',
+        badgeNew: 'on',
     },
     {
         pid: 2,
-        pname: '브리즈 라운드 테이블',
-        pdesc: `자연스러운 우드 컬러와 부드러운 곡선이 어우러진 원형 테이블`,
-        price: 1420000,
-        pdiscount: 0.2,
-        priceDiscount: 1136000,
-        plike: 73,
-        pthumbFileName: 'con3-3.jpg'
+        pname: '글래스팅 멜팅 밤',
+        price: 13000,
+        pdiscount: 0.28,
+        priceDiscount: 9400,
+        plipImgName: 'lip-list-img-3.jpg',
+        plipModelName: 'lip-list-model-img-3.jpg',
+        // 상품별 컬러칩 생성과 +Color 개수 계산에 사용하는 색상 데이터
+        pcolors: [
+            '#82484B', '#9C5858', '#BA6F6A', '#D68D82', '#ECD6D1',
+            '#C77965', '#E3A18B',
+        ],
+        badgeBest: 'off',
+        badgeNew: 'on',
     },
     {
         pid: 3,
-        pname: '린넨 데이베드 소파',
-        pdesc: `가볍고 편안한 린넨 소재로 완성한 여유로운 휴식 공간`,
-        price: 1890000,
-        pdiscount: 0.3,
-        priceDiscount: 1323000,
-        plike: 98,
-        pthumbFileName: 'con3-4.jpg'
+        pname: '롬앤 더 쥬시 래스팅 틴트',
+        price: 13000,
+        pdiscount: 0.28,
+        priceDiscount: 9400,
+        plipImgName: 'lip-list-img-4.jpg',
+        plipModelName: 'lip-list-model-img-4.jpg',
+        // 상품별 컬러칩 생성과 +Color 개수 계산에 사용하는 색상 데이터
+        pcolors: [
+            '#78383F', '#91434B', '#A94F58', '#C3636D', '#DC8790',
+            '#641F2A', '#762532', '#8A2D3B', '#9D3746', '#B34352',
+            '#C75260', '#D66370', '#E17682', '#EC8D98', '#F2A6AF',
+            '#6C3338', '#824146', '#995157', '#AF636A', '#C5787F',
+            '#DA9298', '#E8ADB2', '#F1CDD0',
+        ],
+        badgeBest: 'off',
+        badgeNew: 'on',
     },
-    {
-        pid: 4,
-        pname: '루프 라탄 체어',
-        pdesc: `자연스러운 라탄 소재와 원목 프레임으로 완성한 감각적인 체어`,
-        price: 520000,
-        pdiscount: 0.25,
-        priceDiscount: 390000,
-        plike: 57,
-        pthumbFileName: 'con3-5.jpg'
-    },
-    {
-        pid: 5,
-        pname: '무드 오크 암체어',
-        pdesc: `따뜻한 오크 소재와 부드러운 곡선으로 공간에 편안함을 더하는 의자`,
-        price: 760000,
-        pdiscount: 0.35,
-        priceDiscount: 494000,
-        plike: 112,
-        pthumbFileName: 'con3-6.jpg'
-    },
-]
+];
