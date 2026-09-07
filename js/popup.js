93ca789274038472aeed7596546e0f7b2d3bc2bc
ycr <chaerin7272@gmail.com>
2026-09-07 14:58:20 +0900

    메인화면 팝업추가

---
+++ js/popup.js
@@ -0,0 +1,23 @@
+const popup = document.querySelector('.popup');
+const modal = document.querySelector('.modal');
+const popupClose = document.querySelector('#popup-close');
+
+const lockBodyScroll = () => {
+  document.body.style.overflow = 'hidden';
+};
+
+const unlockBodyScroll = () => {
+  document.body.style.overflow = '';
+};
+
+if (popup && modal && !popup.classList.contains('off') && !modal.classList.contains('off')) {
+  lockBodyScroll();
+}
+
+if (popupClose) {
+  popupClose.addEventListener('click', () => {
+    popup.classList.add('off');
+    modal.classList.add('off');
+    unlockBodyScroll();
+  });
+}
