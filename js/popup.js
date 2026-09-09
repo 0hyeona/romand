4c6255bb39681a80dc93ca978ce3ca78c909a0d4
hyeona <01.hyeona@gmail.com>
2026-09-10 01:50:56 +0900

    fix: 오늘 하루 보지않기 로직 구현

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,67 @@
 const popup = document.querySelector('.popup');
 const modal = document.querySelector('.modal');
-const popupClose = document.querySelector('.popup-close');
 
+const popupClose = document.querySelector('.popup-close');
+const popupTodayClose = document.querySelector('.popup-today-close');
+
+// 팝업 뒤쪽 스크롤 막기 - 팝업 생성 시
 const lockBodyScroll = () => {
   document.body.style.overflow = 'hidden';
 };
 
+// 팝업 뒤쪽 스크롤 해제 - 팝업 없어졌을 시 
 const unlockBodyScroll = () => {
   document.body.style.overflow = '';
 };
 
-if (popup && modal && !popup.classList.contains('off') && !modal.classList.contains('off')) {
+// 오늘 날짜
+const today = new Date();
+const todayDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+
+// 저장된 날짜
+const hideDate = localStorage.getItem('hideDate');
+
+
+// 오늘 하루 보지않기를 누른 적이 없을 때만 팝업 보여주기
+if (
+  popup &&
+  modal &&
+  hideDate !== todayDate &&
+  !popup.classList.contains('off') &&
+  !modal.classList.contains('off')
+) {
   lockBodyScroll();
 }
 
+
+// 그냥 닫기
 if (popupClose) {
   popupClose.addEventListener('click', () => {
     popup.classList.add('off');
     modal.classList.add('off');
+
     unlockBodyScroll();
   });
+}
+
+
+// 오늘 하루 보지않기
+if (popupTodayClose) {
+  popupTodayClose.addEventListener('click', () => {
+
+    // 오늘 하루 보지않기를 클릭했을 때 오늘 날짜를 브라우저 hideDate에 저장
+    localStorage.setItem('hideDate', todayDate);
+
+    popup.classList.add('off');
+    modal.classList.add('off');
+
+    unlockBodyScroll();
+  });
+}
+
+
+// 이미 오늘 하루 보지않기를 눌렀다면
+if (hideDate === todayDate) {
+  popup.classList.add('off');
+  modal.classList.add('off');
 }
